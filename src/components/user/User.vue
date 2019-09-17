21c17f9387e92de93f76794e45f763dc01ea5c3b
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2019-09-17 16:51:23 +0600

    Reactive router link added.

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,15 +2,32 @@
   <div>
     <h1>The User Page</h1>
     <hr />
-    <button @click="navigateToHome">Go to Home</button>
+    <p>Loaded ID: {{ id }} (watch)</p>
+    <p>Loaded ID: {{ uid }} (computed)</p>
+    <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    };
+  },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id;
+    }
+  },
+  computed: {
+    uid() {
+      return this.$route.params.id;
+    }
+  },
   methods: {
     navigateToHome() {
-      this.$router.push();
+      this.$router.push("/"); // Similiar to React's history.push()
     }
   }
 };
