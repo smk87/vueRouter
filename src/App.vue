333e09666187d62f7e7e3cbe9cdb13cedda2a244
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2019-09-20 12:27:08 +0600

    Route animation, hash scrolling added.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
-        <hr />
+        <hr>
         <router-view name="header-top"></router-view>
-        <router-view></router-view>
+        <transition name="slide" mpde="out-in">
+          <router-view></router-view>
+        </transition>
         <router-view name="header-bottom"></router-view>
       </div>
     </div>
@@ -23,4 +25,45 @@ export default {
 </script>
 
 <style>
+.slide-enter {
+  opacity: 0;
+  /* transform: translateY(20px); */
+}
+
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
+
+.slide-leave {
+}
+
+.slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  transition: opacity 3s;
+  opacity: 0;
+  position: absolute;
+}
+
+.slide-move {
+  transition: transform 1s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -9,7 +9,16 @@ Vue.use(VueRouter);
 // Add the routes
 const router = new VueRouter({
   routes,
-  mode: "history"
+  mode: "history",
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  }
 });
 
 new Vue({
