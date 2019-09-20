a915bb0b1047ce2de5456b197b6b7a650f761be9
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2019-09-20 11:48:40 +0600

    Named routerr-view added.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
         <hr />
-        <Header />
         <router-view name="header-top"></router-view>
         <router-view></router-view>
         <router-view name="header-bottom"></router-view>
